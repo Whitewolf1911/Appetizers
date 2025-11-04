869d627cb5947ef604d6ee12d923fa35941f9bfd
Ali Basoglu <alibasoglu1911@gmail.com>
2025-11-04 10:34:31 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ali Basoglu on 4.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
